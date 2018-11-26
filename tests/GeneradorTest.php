c0a8d12a3ba639f2c61765d7e9d8d7871107fc41
Mauro Tebay <maurotebay@gmail.com>
2018-11-25 21:55:52 -0300

    Comentado tests

--- tests/GeneradorTest.php
+++ tests/GeneradorTest.php
@@ -10,25 +10,29 @@ class GeneradorTest extends TestCase {
     
     public function testGeneradoCorrecto() {
         
-        //Generamos un array a partir del archivo
+        //Generamos un objeto de tipo generador que dentro tiene un array de la cantidad de preguntas que nosotros prefieramos
         $generador = new Generador(dirname(__DIR__) . '/preguntas.yml', 10);
 
-        //Probamos que se genere
+        //Probamos que se exista el nuevo generador
         $this->assertTrue(isset($generador));
     }
 
     public function testObtenerPreguntas(){
 
+        //Creamos un nuevo generador con una sola pregunta
         $generador = new Generador(dirname(__DIR__) . '/preguntas.yml', 1);
 
+        //Comprobamos que exista la pregunta en el generador
         $this->assertNotNull($generador->verPreguntas());
         
     }
 
     public function testCantPreguntas(){
 
+        //Creamos un generador con 17 preguntas
         $generador = new Generador(dirname(__DIR__) . '/preguntas.yml', 17);
 
+        //Comprobamos que obtener cantidad de preguntas devuelve la cantidad de preguntas pedida
         $this->assertEquals($generador->obtenerCantPreguntas(), 17);
     }
 }
--- tests/PreguntaTest.php
+++ tests/PreguntaTest.php
@@ -9,20 +9,31 @@ class PreguntaTest extends TestCase {
 
     public function testPreguntaExiste(){
 
+        //En la variable archivo guardamos el archivo preguntas.yml convertido en array
         $archivo = Yaml::parseFile(dirname(__DIR__) . '/preguntas.yml');
+
+        //Guardamos el array de preguntas en una variable aparte
         $arrayPreguntas = $archivo['preguntas'];
 
+        //Creamos una pregunta a partir del primero objeto del array de preguntas disponible
         $pregunta = new Pregunta($arrayPreguntas[0], 1);
 
+        //Comprobamos que se haya creado correctamente el objeto pregunta
         $this->assertTrue(isset($pregunta));
     }
 
     public function testObtenerDatosPregunta(){
-        $archivo = Yaml::parseFile(dirname(__DIR__) . '/preguntas.yml');
-        $arrayPreguntas = $archivo['preguntas'];
 
+        //En la variable archivo guardamos el archivo preguntas.yml convertido en array
+        $archivo = Yaml::parseFile(dirname(__DIR__) . '/preguntas.yml');
+
+        //Guardamos el array de preguntas en una variable aparte
+        $arrayPreguntas = $archivo['preguntas'];
+        
+        //Creamos una pregunta a partir del primero objeto del array de preguntas disponible
         $pregunta = new Pregunta($arrayPreguntas[0], 1);
 
+        //Comprobamos que existen todas las caracteristicas de la pregunta
         $this->assertNotNull($pregunta->obtenerDescripcion());
         $this->assertNotNull($pregunta->obtenerNumero());
         $this->assertNotNull($pregunta->obtenerRespCorrectas());
